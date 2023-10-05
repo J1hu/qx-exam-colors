31b4704f356d6e59891f1c9719078ca54537b5b9
Jehu Casimiro <jehu.casimiro16@gmail.com>
2023-10-05 15:52:11 +0800

    added TSX code

---
+++ src/App.tsx
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Stack, ListGroup } from "react-bootstrap";
+
+// Define the types for the details object
+interface Details {
+  name: string;
+  hex: string;
+  code: string;
+}
+
+// Parent Component
+function Colors() {
+  const [colors, setColors] = useState<any[]>([]); // Use 'any[]' as a temporary fix for the type of colors
+  const [details, setDetails] = useState<Details>({ name: "", hex: "", code: "" });
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      // Fetch data from the API
+      try {
+        const response = await fetch("https://api.prolook.com/api/colors/prolook");
+
+        // Throw error response
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status ${response.status}`);
+        }
+
+        // Put the data to the state
+        const data = await response.json();
+        setColors(data);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    }
+
+    // Calling the async function
+    fetchData();
+  }, []);
+
+  // HELPERS
+  function getDetails(name: string, hex: string, code: string) {
+    setDetails({ name, hex, code });
+  }
+
+  // Function to calculate contrast ratio
+  function calculateContrastRatio(hexColor: string): number {
+    const hexToRgb = (hex: string) => {
+      const r = parseInt(hex.slice(0, 2), 16);
+      const g = parseInt(hex.slice(2, 4), 16);
+      const b = parseInt(hex.slice(4, 6), 16);
+      return [r, g, b];
+    };
+
+    const [r, g, b] = hexToRgb(hexColor);
+
+    // Calculate luminance using the relative luminance formula
+    const luminance =
+      0.2126 * (r / 255) + 0.7152 * (g / 255) + 0.0722 * (b / 255);
+
+    // Calculate contrast ratio
+    const contrast = (luminance + 0.05) / 0.05;
+
+    return contrast;
+  }
+
+  // COMPONENTS
+  interface ColorListProps {
+    colors: any; // Use 'any' as a temporary fix for the type of colors
+  }
+
+  function ColorList({ colors }: ColorListProps) {
+    // Validation, can also be used as a loading screen when fetching data
+    if (!colors || !Array.isArray(colors.colors)) {
+      return <div className="center wrapper fs-4">Loading...</div>;
+    }
+
+    // Setting the visibility to true when the user selects a color
+    const toggleVisibility = () => {
+      setIsVisible(true);
+    };
+
+    // ColorList render
+    return (
+      <div className="color-list-wrapper">
+        {colors.colors.map((color: any) => (
+          <Stack key={color.name}>
+            <ListGroup>
+              <ListGroup.Item className="d-flex justify-content-between align-middle">
+                <p className="fs-5">{color.name}</p>
+                <Button
+                  as="a"
+                  variant="primary"
+                  onClick={() => {
+                    // Getting the details to pass to the ColorPreview component
+                    getDetails(color.name, color.hex_code, color.color_code);
+                    // Toggle visibility
+                    toggleVisibility();
+                  }}
+                >
+                  Preview
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Stack>
+        ))}
+      </div>
+    );
+  }
+
+  // Preview the selected color
+  interface ColorPreviewProps {
+    details: Details;
+    isVisible: boolean;
+  }
+
+  function ColorPreview({ details, isVisible }: ColorPreviewProps) {
+    // Handle if the user has not yet selected a color
+    if (!isVisible) {
+      return <div className="color-preview fs-4">Please select a color</div>;
+    }
+
+    // Set text color based on the background
+    const contrastRatio = calculateContrastRatio(details.hex);
+    let textColor = "black";
+
+    if (contrastRatio < 4.5) {
+      textColor = "white";
+    }
+
+    // ColorPreview render
+    return (
+      <div
+        className="color-preview"
+        style={{ backgroundColor: `#${details.hex}`, color: textColor }}
+      >
+        <div className="fs-5 text-center">
+          <p>Name: {details.name}</p>
+          <p>Hex: {details.hex}</p>
+          <p>Code: {details.code}</p>
+        </div>
+      </div>
+    );
+  }
+
+  // Parent Component render
+  return (
+    <div className="whole-wrapper">
+      <h1>Colors:</h1>
+      <div className="wrapper">
+        <ColorList colors={colors} />
+        <ColorPreview details={details} isVisible={isVisible} />
+      </div>
+    </div>
+  );
+}
+
+export default Colors;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Colors from './Colors.jsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Colors />
+    <App />
   </React.StrictMode>,
 )
